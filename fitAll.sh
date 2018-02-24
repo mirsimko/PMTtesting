6361cc331e9ced857efd551adfa8e3262dd3120a
Miroslav Simko <mirsimko@gmail.com>
2018-02-23 23:37:17 -0500

    pdfs are merged automatically after they are created

--- fitAll.sh
+++ fitAll.sh
@@ -5,3 +5,6 @@
 for txtFile in gainPerformance/*.txt; do
   root -l -b -q fitPmtYield.C+'("'"$txtFile"'")'
 done
+
+# merge the output pdfs
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=allPmts.pdf gainPerformance/*.pdf
