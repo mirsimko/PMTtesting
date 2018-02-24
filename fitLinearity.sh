ea339f078d9e78e37ba1f38dedfec93b3be0c02a
Miroslav Simko <mirsimko@gmail.com>
2018-02-23 23:47:17 -0500

    small changes

--- fitLinearity.sh
+++ fitLinearity.sh
@@ -2,9 +2,9 @@
 
 read -p 'supply voltage in Volts? ' voltage
 
-for txtFile in linearityInputs/$voltage/*.txt; do
+for txtFile in linearityInputs/"$voltage"/*.txt; do
   root -l -b -q fitLinearity.C+'("'"$txtFile"'")'
 done
 
 # merge the output pdfs
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=allPmts.pdf linearityInputs/*.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=allPmts.pdf linearityInputs/"$voltage"*.pdf
